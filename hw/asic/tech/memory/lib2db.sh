cdb013ed9099b9388897b845ff5ea8941f742380
luigi.giuffrida <luigi.giuffrida@polito.it>
2025-01-13 17:01:48 +0100

    Add tech files

---
+++ hw/asic/tech/memory/lib2db.sh
@@ -0,0 +1,5 @@
+read_lib sky130_sram_4kbyte_1rw_32x1024_8_TT_1p8V_25C.lib
+
+report_lib sky130_sram_4kbyte_1rw_32x1024_8_TT_1p8V_25C_lib
+
+write_lib sky130_sram_4kbyte_1rw_32x1024_8_TT_1p8V_25C_lib -format db -output sky130_sram_4kbyte_1rw_32x1024_8_TT_1p8V_25C.db
